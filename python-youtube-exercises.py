1e8e194bfbd5cc6d2df328b7b2e4df496c31cec9
ptd3v <tovey@live.co.uk>
2023-08-11 22:40:21 +0100

    Update python-youtube-exercises.py

--- python-youtube-exercises.py
+++ python-youtube-exercises.py
@@ -1,2 +1,16 @@
+#Variables - Used to store temporary data in a computers memory
+age = 30
+age = 20 #Variable are run from top to bottom
+#Underscores help readability
+first_name = "John"
+last_name = "Smith"
+new_patient = True #Boolean values can be either true or false, Case sensistive
+price = 19.95
+print(age)
+
 #String - Textual Data
 print("Hello World!")
+
+#Taking a name and storing it as a variable
+name = input("What is your name? ")
+print("Hello " + name)
